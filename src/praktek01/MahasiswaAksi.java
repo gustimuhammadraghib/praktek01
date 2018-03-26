efb05ae402772f59e81a0a901b425f65219bf8be
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-03-27 07:28:01 +0800

    Mengisi Nilai Atribut

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -12,6 +12,10 @@ package praktek01;
 public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Harry = new Mahasiswa();
+         
+        Harry.nama="Harry Potter";
+        Harry.NPM="16630904";
+        Harry.IPK=3,4;
         
     }
   
